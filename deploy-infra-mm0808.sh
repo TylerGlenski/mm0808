24923f7c2c8f14f52c4e65386fd2e7df54e907d5
Tyler Glenski <tglenski1@gmail.com>
2020-08-09 16:41:24 -0500

    CodePipeline Github CI/CD added yml and sh files

--- deploy-infra-mm0808.sh
+++ deploy-infra-mm0808.sh
@@ -6,8 +6,43 @@ CLI_PROFILE=mm0808
 
 EC2_INSTANCE_TYPE=t2.micro
 
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile mm0808 \
+        --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# create access token for git
+# set git variables
+GH_ACCESS_TOKEN=$(cat ~/.github/mm0808-access-token)
+GH_OWNER=$(cat ~/.github/mm0808-owner)
+GH_REPO=$(cat ~/.github/mm0808-repo)
+GH_BRANCH=main
+
+echo Vars created so far:
+echo GH_ACCESS_TOKEN = $GH_ACCESS_TOKEN
+echo GH_OWNER = $GH_OWNER
+echo GH_REPO = $GH_REPO
+echo GH_BRANCH = $GH_BRANCH
+echo CODEPIPELINE_BUCKET = $CODEPIPELINE_BUCKET
+echo AWS_ACCOUNT_ID = $AWS_ACCOUNT_ID
+echo EC2_INSTANCE_TYPE = $EC2_INSTANCE_TYPE
+echo CLI_PROFILE = $CLI_PROFILE
+echo REGION = $REGION
+echo STACK_NAME = $STACK_NAME
+
+# Deploy static resources
+echo -e "\n\n========== Deploying setup.yml static  resources =========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 # Deploy the CloudFormation template
-echo -e "\n\n=========== Deploying main.yml ==========="
+echo -e "\n\n========== Deploying main.yml =========="
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
@@ -16,7 +51,13 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    EC2InstanceType=$EC2_INSTANCE_TYPE
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitOwner=$GH_OWNER \
+    GitRepo=$GH_REPO \
+    GitBranch=$GH_BRANCH \
+    GitAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 
 # if the deploy succeeded, show DNS name of created instance
 if [ $? -eq 0 ]; then
