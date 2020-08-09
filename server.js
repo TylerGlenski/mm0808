b06e815e750fab284dbefa3ea5f2f3ba966a2a1b
Tyler Glenski <tglenski1@gmail.com>
2020-08-09 17:04:29 -0500

    Changed message

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { hostname } = require("os");
 const http = require("http");
-const message = "Hello from test branch\n";
+const message = "Hello from from cloud and github\n";
 const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
